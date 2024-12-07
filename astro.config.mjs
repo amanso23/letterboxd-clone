7006e2738003bf0b958f10f980e357763740c228
Adrian <adrianmanso80@gmail.com>
2024-12-07 08:03:51 +0000

    Add Node adapter and enable SVG sprite mode in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,19 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+
   experimental: {
     svg: {
       mode: 'sprite'
     }
-  }
+  },
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
